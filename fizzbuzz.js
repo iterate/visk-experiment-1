c8e1201ced49cb8a2f917facc7d6b2bdb7c39429
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2018-12-19 15:05:16 +0100

    Før du sjekker om number er delelig på 3, sjekk om det er delelig på 3 og 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,9 @@
 const assert = require('assert');
 
 function fizzbuzz(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+
+    }
     if (number % 3 === 0) {
         return "Fizz";
     } else if (number % 5 === 0) {
