bc3dad0967a38b281e41c2c96060a9cda3e0ca1f
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2018-12-19 15:10:42 +0100

    Der hvor funksjonen tester om number er delelig på 3, det skal skje bare hvis den forrige sjekken ikke matcher

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,8 +3,7 @@ const assert = require('assert');
 function fizzbuzz(number) {
     if (number % 3 === 0 && number % 5 === 0) {
         return "FizzBuzz!";
-    }
-    if (number % 3 === 0) {
+    } else if (number % 3 === 0) {
         return "Fizz";
     } else if (number % 5 === 0) {
         return "Buzz";
