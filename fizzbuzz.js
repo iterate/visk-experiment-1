e1b394ee624c5b70d1ade88d5d7781963a75dae9
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2018-12-19 15:06:51 +0100

    Hvis det er delelig på

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 function fizzbuzz(number) {
     if (number % 3 === 0 && number % 5 === 0) {
-
+        return "FizzBuzz!";
     }
     if (number % 3 === 0) {
         return "Fizz";
