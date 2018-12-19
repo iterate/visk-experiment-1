3c1ba52f8cb220b355daba63aed66e8689e97f12
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2018-12-19 15:02:19 +0100

    *Flytt returnering av number til sist i funksjonen*

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 
 function fizzbuzz(number) {
-    return number;
     if (number % 3 === 0) {
         return "Fizz";
     } else if (number % 5 === 0) {
         return "Buzz";
     }
+    return number;
 }
 
 assert(fizzbuzz(1) === 1);
