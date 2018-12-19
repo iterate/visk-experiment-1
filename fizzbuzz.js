486bf8443b89ad46a6349f053d5710b7e48ae2c2
Henrik Glasø Skifjeld <henrikgs@gmail.com>
2018-12-19 14:51:24 +0100

    Ellers, hvis det er delelig på 5, skriv ut Buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,5 +2,7 @@ function fizzbuzz(number) {
     console.log(number);
     if (number % 3 === 0) {
         console.log("Fizz");
+    } else if (number % 5 === 0) {
+        console.log("Buzz");
     }
 }
